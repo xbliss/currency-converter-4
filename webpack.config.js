c206de1044c8f9f2bcc39c7c9e2cbb901dd5e019
Zach Eagle <mazaxakep@gmail.com>
2016-03-19 00:08:39 +0300

    react bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+var cssnext = require('postcss-cssnext')
+var fontMagician = require('postcss-font-magician')
+var postcssImport = require('postcss-import')
+
 module.exports = {
   output: {
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
@@ -8,7 +12,20 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel'
+      },
+      {
+        test: /\.sss$/,
+        loader: 'style!css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss?parser=sugarss'
       }
+    ]
+  },
+  postcss: function (webpack) {
+    return [
+      postcssImport({
+        addDependencyTo: webpack
+      }),
+      cssnext,
+      fontMagician
     ]
   }
 }
