611913d8b4e243c6d69681683bd7d42f9eef0f08
Zach Eagle <mazaxakep@gmail.com>
2016-03-19 00:15:17 +0300

    index file for popup

---
+++ src/scripts/popup/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Store } from 'react-chromex-redux'
+import Popup from './components/Popup'
+
+const store = new Store({
+  portName: 'rates'
+})
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Popup />
+  </Provider>
+), document.getElementById('popup'))
