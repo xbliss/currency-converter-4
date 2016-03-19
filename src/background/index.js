e8223dfad288e0f12dfa1d32c615f1b3c8dcb052
Zach Eagle <mazaxakep@gmail.com>
2016-03-19 15:11:38 +0300

    temporarily remove background page because of twice rendering bug

--- src/background/index.js
+++ src/background/index.js
@@ -1,7 +1,7 @@
-import { wrapStore } from 'react-chromex-redux'
-import { createStore } from 'redux'
-import reducer from '../shared/reducer'
-
-const store = createStore(reducer)
-
-wrapStore(store, { portName: 'rates' })
+// import { wrapStore } from 'react-chromex-redux'
+// import { createStore } from 'redux'
+// import reducer from '../shared/reducer'
+//
+// const store = createStore(reducer)
+//
+// wrapStore(store, { portName: 'rates' })
--- src/popup/index.js
+++ src/popup/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { Store } from 'react-chromex-redux'
+import { createStore } from 'redux'
+// import { Store } from 'react-chromex-redux'
 import Popup from './components/Popup/Popup.js'
+import reducer from '../shared/reducer'
 
-const store = new Store({
-  portName: 'rates'
-})
+// const store = new Store({
+//   portName: 'rates'
+// })
+
+const store = createStore(reducer)
 
 ReactDOM.render((
   <Provider store={store}>
