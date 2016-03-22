16f94fd3d5a497a38950ac40a3d04ac31f74268b
Zach Eagle <mazaxakep@gmail.com>
2016-03-22 21:08:24 +0300

    remove comments

--- src/popup/index.js
+++ src/popup/index.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import { Store } from 'react-chromex-redux'
 import Popup from './components/Popup/Popup.js'
 import reducer from '../shared/reducer'
-
-// const store = new Store({
-//   portName: 'rates'
-// })
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
