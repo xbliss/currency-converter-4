4018c4cc7e65506d0cf1dd5a068fa75ac33e6037
Zach Eagle <mazaxakep@gmail.com>
2016-03-31 04:13:36 +0300

    remove unnecessary crossorigin proxy

--- src/shared/actions.js
+++ src/shared/actions.js
@@ -29,7 +29,7 @@ export function fetchData () {
       return dispatch(loadData(data.usd, data.eur))
     } else {
       dispatch(requestData())
-      return fetch('https://crossorigin.me/https://meduza.io/api/v3/stock/all/')
+      return fetch('https://meduza.io/api/v3/stock/all/')
         .then((response) => {
           return response.json()
         })
