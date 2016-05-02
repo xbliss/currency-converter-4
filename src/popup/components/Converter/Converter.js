3ba0c356d5aa9f50b267ab8358ed0d892c4c8b6f
Zach Orlovsky <sadorlovsky@gmail.com>
2016-05-02 20:15:28 +0300

    swapping icons

--- src/popup/components/Converter/Converter.js
+++ src/popup/components/Converter/Converter.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
 import FaArrowLeft from 'react-icons/lib/fa/arrow-left'
 import FaArrowRight from 'react-icons/lib/fa/arrow-right'
+import FaUsd from 'react-icons/lib/fa/dollar'
+import FaEur from 'react-icons/lib/fa/eur'
+import FaRouble from 'react-icons/lib/fa/rouble'
 import { isNaN, toNumber } from 'lodash'
 import styles from './Converter.sss'
 
@@ -42,16 +45,28 @@ export default class Converter extends Component {
   convert () {
     const rate = this.props[this.props.current.toLowerCase()]
     if (!this.props.swap) {
-      return this.state.input * rate
+      const result = this.state.input * rate
+      return this.props.accuracy ? result.toFixed(2) : Math.round(result)
     } else {
-      return this.state.input / rate
+      return this.reverseConvert()
     }
   }
 
+  reverseConvert () {
+    const result = this.state.input / this.props[this.props.current.toLowerCase()]
+    if ((result > 0) && (result < 1)) {
+      return '< 1'
+    }
+    return result.toFixed(2)
+  }
+
   render () {
-    const value = () => this.props.accuracy ? this.convert().toFixed(2) : Math.round(this.convert())
+    const currentIcon = this.props.current === 'USD' ? <FaUsd /> : <FaEur />
     return (
       <div styleName='converter'>
+        <div styleName='icon'>
+          {!this.props.swap ? currentIcon : <FaRouble />}
+        </div>
         <input
           autoFocus
           placeholder='0'
@@ -63,7 +78,10 @@ export default class Converter extends Component {
           <FaArrowLeft styleName='left' />
           <FaArrowRight styleName='right' />
         </div>
-        <input styleName='input' value={value()} />
+        <input styleName='input' value={this.convert()} />
+        <div styleName='icon'>
+          {this.props.swap ? currentIcon : <FaRouble />}
+        </div>
       </div>
     )
   }
