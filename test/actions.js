434e236db6ecd831f3f897b3c4fba8d1b9233f6d
Zach Orlovsky <sadorlovsky@gmail.com>
2016-05-07 17:30:35 +0300

    update test

--- test/actions.js
+++ test/actions.js
@@ -1,41 +1,47 @@
 import test from 'ava'
 import * as actions from '../src/shared/actions'
 
-test('toggle currency to usd', (t) => {
+test('toggle currency to usd', t => {
   const expected = {
     type: 'TOGGLE_CURRENCY',
-    currency: 'USD'
+    payload: {
+      currency: 'USD'
+    }
   }
-  t.same(actions.toggleCurrency('USD'), expected)
+  t.deepEqual(actions.toggleCurrency('USD'), expected)
 })
 
-test('toggle currency to eur', (t) => {
+test('toggle currency to eur', t => {
   const expected = {
     type: 'TOGGLE_CURRENCY',
-    currency: 'EUR'
+    payload: {
+      currency: 'EUR'
+    }
   }
-  t.same(actions.toggleCurrency('EUR'), expected)
+  t.deepEqual(actions.toggleCurrency('EUR'), expected)
 })
 
-test('toggle accuracy', (t) => {
+test('toggle accuracy', t => {
   const expected = {
     type: 'TOGGLE_ACCURACY'
   }
-  t.same(actions.toggleAccuracy(), expected)
+  t.deepEqual(actions.toggleAccuracy(), expected)
 })
 
-test('request data', (t) => {
+test('request data', t => {
   const expected = {
     type: 'REQUEST_DATA'
   }
-  t.same(actions.requestData(), expected)
+  t.deepEqual(actions.requestData(), expected)
 })
 
-test('load data', (t) => {
+test('load data', t => {
   const expected = {
     type: 'LOAD_DATA',
-    usd: 33,
-    eur: 45
+    payload: {
+      usd: 33,
+      eur: 45
+    }
   }
-  t.same(actions.loadData(33, 45), expected)
+  t.deepEqual(actions.loadData(33, 45), expected)
 })
