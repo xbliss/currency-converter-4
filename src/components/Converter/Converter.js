2823c758777faefd5e06bc81bcfc833c8565ccd7
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-22 01:42:59 +0300

    change Converter component to functional component

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,74 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
 import css from 'react-css-modules'
+import withState from 'recompose/withState'
+import compose from 'recompose/compose'
 import FaArrowLeft from 'react-icons/lib/fa/arrow-left'
 import FaArrowRight from 'react-icons/lib/fa/arrow-right'
 import styles from './Converter.css'
 import CurrencyIcon from './CurrencyIcon'
 
-@css(styles)
-export default class Converter extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      input: 0
-    }
-  }
-
-  handleChange (value) {
-    if (!isNaN(value)) {
-      this.setState({
-        input: Number(value)
-      })
-    }
-  }
-
-  handleBlur (target) {
+const Converter = props => {
+  const handleBlur = target => {
     setTimeout(() => {
       target.focus()
     }, 0)
   }
 
-  handleClick () {
-    this.props.doSwap()
+  const handleChange = value => {
+    props.setInput(() => {
+      return Number(value)
+    })
   }
 
-  convert () {
+  const handleClick = () => props.doSwap()
+
+  const convert = () => {
     const result = () => {
-      return this.props.swap ?
-      this.state.input / this.props[this.props.current.toLowerCase()] :
-      this.state.input * this.props[this.props.current.toLowerCase()]
+      return props.swap ?
+      props.input / props[props.current.toLowerCase()] :
+      props.input * props[props.current.toLowerCase()]
     }
     const accuracy = value => {
-      return this.props.accuracy ? value.toFixed(2) : Math.round(value)
+      return props.accuracy ? value.toFixed(2) : Math.round(value)
     }
-    return this.props.accuracy ? result() : accuracy(result())
+    return props.accuracy ? result() : accuracy(result())
   }
 
-  render () {
-    return (
-      <div styleName='converter'>
-        <CurrencyIcon
-          currency={this.props.current}
-          swap={this.props.swap}
-          convertToRUB
-        />
-        <input
-          autoFocus
-          placeholder='0'
-          styleName='input'
-          onChange={e => this.handleChange(e.target.value)}
-          onBlur={e => this.handleBlur(e.target)}
-        />
-        <div styleName='swap' onClick={() => this.handleClick()}>
-          <FaArrowLeft styleName='left' />
-          <FaArrowRight styleName='right' />
-        </div>
-        <input styleName='input' value={this.convert()} />
-        <CurrencyIcon
-          currency={this.props.current}
-          swap={this.props.swap}
-        />
+  return (
+    <div styleName='converter'>
+      <CurrencyIcon
+        currency={props.current}
+        swap={props.swap}
+        convertToRUB
+      />
+      <input
+        autoFocus
+        placeholder='0'
+        styleName='input'
+        onChange={e => handleChange(e.target.value)}
+        onBlur={e => handleBlur(e.target)}
+      />
+      <div styleName='swap' onClick={() => handleClick()}>
+        <FaArrowLeft styleName='left' />
+        <FaArrowRight styleName='right' />
       </div>
-    )
-  }
+      <input styleName='input' value={convert()} />
+      <CurrencyIcon
+        currency={props.current}
+        swap={props.swap}
+      />
+    </div>
+  )
 }
+
+const enhance = compose(
+  withState('input', 'setInput', 0),
+  css(styles)
+)
+
+export default enhance(Converter)
