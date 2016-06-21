bb74a443809e53bfec5edf6e34357f35e25a6b25
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-22 00:23:56 +0300

    update webpack configs

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -17,6 +17,12 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        include: /node_modules/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader'
       }
     ]
