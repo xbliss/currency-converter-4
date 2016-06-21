12c9077b4bf73f070e9cb8c1d41672582993c306
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-22 00:37:56 +0300

    change Rate component to functional component

--- src/components/Rate/Rate.js
+++ src/components/Rate/Rate.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import css from 'react-css-modules'
+import lifecycle from 'recompose/lifecycle'
+import compose from 'recompose/compose'
 import Spinner from 'react-spinkit'
 import FaRouble from 'react-icons/lib/fa/rouble'
 import styles from './Rate.css'
 
-@css(styles)
-export default class Rate extends Component {
-  componentDidMount () {
-    this.props.fetchData()
-  }
-
-  render () {
-    const showSpinner = () => <Spinner spinnerName='three-bounce' noFadeIn />
-    const showValue = () => {
-      const value = this.props.accuracy ? this.props.value.toFixed(2) : Math.round(this.props.value)
-      return (
-        <div onClick={() => this.props.toggleAccuracy()}>
-          {value}
-          <FaRouble styleName='rub' />
-        </div>
-      )
-    }
-
+const Rate = props => {
+  const showSpinner = () => <Spinner spinnerName='three-bounce' noFadeIn />
+  const showValue = () => {
+    const value = props.accuracy ? props.value.toFixed(2) : Math.round(props.value)
     return (
-      <div styleName='rate'>
-        {this.props.isFetching ? showSpinner() : showValue()}
+      <div onClick={() => props.toggleAccuracy()}>
+        {value}
+        <FaRouble styleName='rub' />
       </div>
     )
   }
+
+  return (
+    <div styleName='rate'>
+      {props.isFetching ? showSpinner() : showValue()}
+    </div>
+  )
 }
+
+const enchance = compose(
+  lifecycle({
+    componentDidMount () {
+      this.props.fetchData()
+    }
+  }),
+  css(styles)
+)
+export default enchance(Rate)
